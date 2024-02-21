277cea4e6ab4cd5b7263d3dbfb1a8fcfc46346a4
Eric G. Wolfe <eric.wolfe@microsoft.com>
2024-02-20 23:47:22 -0500

    v4.1.0
    Update cookbook to render nfs.conf template.
    Picked upstream changes, but maintained CycleCloud Jetpack compatibility

--- metadata.rb
+++ metadata.rb
@@ -4,18 +4,18 @@ license 'Apache-2.0'
 description 'Installs and configures NFS, and NFS exports'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 name 'nfs'
-version '3.0.2'
-source_url 'https://github.com/atomic-penguin/cookbook-nfs' if respond_to?(:source_url)
-issues_url 'https://github.com/atomic-penguin/cookbook-nfs/issues' if respond_to?(:issues_url)
+version '4.1.0'
+source_url 'https://github.com/atomic-penguin/cookbook-cycle-nfs' if respond_to?(:source_url)
+issues_url 'https://github.com/atomic-penguin/cookbook-cycle-nfs/issues' if respond_to?(:issues_url)
 chef_version '>= 13.0' if respond_to?(:chef_version)
 
-supports 'ubuntu', '>= 15.04'
-supports 'debian', '>= 10.0'
-supports 'centos', '>= 7.0'
-supports 'redhat', '>= 7.0'
-supports 'scientific', '>= 7.0'
-supports 'oracle', '>= 7.0'
-supports 'sles', '>= 11.1'
-supports 'freebsd', '>= 9.3'
+supports 'ubuntu'
+supports 'debian'
+supports 'centos'
+supports 'redhat'
+supports 'scientific'
+supports 'oracle'
+supports 'sles'
+supports 'freebsd'
 
 depends 'line', '~> 2.1'
